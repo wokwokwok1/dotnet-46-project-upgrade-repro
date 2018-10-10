e7081b7887f136de9fc64f1254a0d4d430eb2795
Douglas Linder <Douglas.Linder@civica.com.au>
2018-10-10 11:04:06 +0800

    update to broken packages

--- DemoApp/DemoApp/Controllers/HomeController.cs
+++ DemoApp/DemoApp/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,16 @@ namespace DemoApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HomeControllerService _service;
+
+        public HomeController()
+        {
+            _service = new HomeControllerService();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(_service.Index());
         }
 
         public ActionResult About()
@@ -25,6 +34,32 @@ namespace DemoApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+    }
+
+    public class HomeIndexViewModel
+    {
+        public string PublicString { get; set; }
+        public string PublicHash { get; set; }
+    }
+
+    public class HomeControllerService
+    {
+        public HomeIndexViewModel Index()
+        {
+            return new HomeIndexViewModel()
+            {
+                PublicString = "Hello World",
+                PublicHash = getHashSHA256("Hello World")
+            };
+        }
+
+        private string getHashSHA256(string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            var hasher = new SHA256Managed();
+            var hash = hasher.ComputeHash(bytes);
+            return hash.Aggregate(string.Empty, (current, x) => current + $"{x:x2}");
         }
     }
 }
